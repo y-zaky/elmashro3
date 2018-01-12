024bc31200cf2641dd02a9af92a36d622e1b1129
Yahia <zaky20@hotmail.com>
2018-01-12 08:30:15 +0000

    redux store not working

--- src/actions/stuffActions.js
+++ src/actions/stuffActions.js
@@ -10,16 +10,17 @@ export function receiveStuff(json) {
 
 export function fetchStuff() {
   return dispatch => {
-    return fetch(url(), {
+    return fetch(url('https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo'), {
       method: 'GET',
       mode: 'cors',
       credentials: 'include',
       headers: {
-        'x-api-key': apiKey,
+        // 'x-api-key': apiKey,
         'Accept': 'application/json'
       }
     })
     .then(response => response.json())
     .then(json => dispatch(receiveStuff(json)));
+    // .catch(err => console.log(err)) ;
   };
 }
